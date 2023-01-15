dea798a032b49e53c0fe9dd9d57a1768a30fdad8
Matthew Mazzanti <matthew.mazzanti@gmail.com>
2023-01-15 13:04:40 -0500

    Add rust

--- flake.nix
+++ flake.nix
@@ -16,7 +16,36 @@
 
           pythonEnv = pkgs.python3.withPackages (ps: with ps; []);
 
-          demo-pkg = pkgs.callPackage ./default.nix {};
+          demo-drv = ({ stdenv }: stdenv.mkDerivation {
+            name = "demo-pkg";
+            src = ./src;
+            buildInputs = with pkgs; [
+              pythonEnv
+              go
+              rustc
+            ];
+            buildCommand = ''
+              mkdir -p $out/bin
+
+              # Python
+              # Python script builder, since I don't feel like learning setuptools again today
+              pyfile="$out/bin/python-hello"
+              echo "#!${pythonEnv}/bin/python" >> "$pyfile"
+              cat $src/hello.py >> "$pyfile"
+              chmod +x "$pyfile"
+
+              # Go
+              # Need to point some env vars to tmp to avoid build failures
+              export GOCACHE="$TMPDIR/go-cache"
+              export GOPATH="$TMPDIR/go"
+              go build -o $out/bin/go-hello $src/hello.go
+
+              # Rust
+              rustc $src/hello.rs -o $out/rust-hello
+            '';
+          });
+
+          demo-pkg = pkgs.callPackage demo-drv {};
         in
         {
           defaultPackage = demo-pkg;
