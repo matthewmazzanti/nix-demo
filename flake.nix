a27f75381366169d3888c053a988624f054f4f4e
Matthew Mazzanti <matthew.mazzanti@gmail.com>
2023-01-15 12:37:31 -0500

    Adding src

--- flake.nix
+++ flake.nix
@@ -4,20 +4,43 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem
+    let
+      supportedSystems = [ "aarch64-darwin" "x86_64-linux" ];
+      eachSystem = flake-utils.lib.eachSystem supportedSystems;
+    in eachSystem
       (system:
         let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in {
-          devShells.default = with pkgs; mkShell {
-              buildInputs = [
-                nixpkgs-fmt
-              ];
+          pkgs = import nixpkgs {
+            inherit system;
+          };
 
-              shellHook = ''
-                # ...
-              '';
-            };
+          pythonEnv = pkgs.python3.withPackages (ps: with ps; []);
+
+          demo-drv = ({ stdenv }: stdenv.mkDerivation {
+            name = "demo-pkg";
+            src = ./src;
+            buildInputs = [
+              pythonEnv
+            ];
+            buildCommand = ''
+              echo "#!${pythonEnv}" >> hello.py
+              cat $src/hello.py >> hello.py
+              chmod +x hello.py
+            '';
+
+            installCommand = ''
+              mkdir -p $out/bin
+              mv hello.py $out/bin
+            '';
+          });
+
+          demo-pkg = pkgs.callPackage demo-drv {};
+        in
+        {
+          defaultPackage = demo-pkg;
+          devShells.default = with pkgs; mkShell {
+            inputsFrom = [ demo-pkg ];
+          };
         }
       );
 }
